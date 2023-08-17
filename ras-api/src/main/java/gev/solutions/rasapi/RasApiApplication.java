3a8eb122fb20721e13178c58da906609a73b5b9c
Ge <valdez.glv@gmail.com>
2023-08-17 12:07:37 +0800

    intial commit

---
+++ ras-api/src/main/java/gev/solutions/rasapi/RasApiApplication.java
@@ -0,0 +1,13 @@
+package gev.solutions.rasapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RasApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RasApiApplication.class, args);
+	}
+
+}
---
+++ ras-api/src/test/java/gev/solutions/rasapi/RasApiApplicationTests.java
@@ -0,0 +1,13 @@
+package gev.solutions.rasapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RasApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
